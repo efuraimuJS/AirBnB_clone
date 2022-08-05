5c767353ea4439309649253b1582add57c24bbd3
efuraimujs <efuraimu.js@gmail.com>
2022-08-05 18:39:36 +0300

    milestone-05 2022-08-05T15:38:44+00:00

--- models/base_model.py
+++ models/base_model.py
@@ -34,19 +34,19 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
-    
+
     def __str__(self) -> str:
         """Returns a human-readable string representation
         of an instance."""
 
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
-    
+
     def save(self):
         """Updates the public instance attribute updated_at with the current datetime"""
 
         self.updated_at = datetime.now()
         storage.save()
-    
+
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__ of the instance"""
         my_dict = self.__dict__.copy()
