d34ae6517970daff7c58481f1ee34ba4f6747ffd
efuraimujs <efuraimu.js@gmail.com>
2022-08-05 22:42:36 +0300

    milestone-06 2022-08-05T19:42:21+00:00

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -49,4 +49,49 @@ class TestFileStorage(unittest.TestCase):
         msg = "object() takes no parameters"
         self.assertEqual(str(e.exception), msg)
     
+    def test_5_attributes(self):
+        """Tests class attributes."""
+        self.resetStorage()
+        self.assertTrue(hasattr(FileStorage, "_FileStorage__file_path"))
+        self.assertTrue(hasattr(FileStorage, "_FileStorage__objects"))
+        self.assertEqual(getattr(FileStorage, "_FileStorage__objects"), {})
+    
+    def help_test_all(self, classname):
+        """Helper tests all() method for classname."""
+        self.resetStorage()
+        self.assertEqual(storage.all(), {})
+
+        o = storage.classes()[classname]()
+        storage.new(o)
+        key = "{}.{}".format(type(o).__name__, o.id)
+        self.assertTrue(key in storage.all())
+        self.assertEqual(storage.all()[key], o)
+    
+    def test_5_all_base_model(self):
+        """Tests all() method for BaseModel."""
+        self.help_test_all("BaseModel")
+
+    def test_5_all_user(self):
+        """Tests all() method for User."""
+        self.help_test_all("User")
+
+    def test_5_all_state(self):
+        """Tests all() method for State."""
+        self.help_test_all("State")
+
+    def test_5_all_city(self):
+        """Tests all() method for City."""
+        self.help_test_all("City")
+
+    def test_5_all_amenity(self):
+        """Tests all() method for Amenity."""
+        self.help_test_all("Amenity")
+
+    def test_5_all_place(self):
+        """Tests all() method for Place."""
+        self.help_test_all("Place")
+
+    def test_5_all_review(self):
+        """Tests all() method for Review."""
+        self.help_test_all("Review")
     
